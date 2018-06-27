821fbf932422690bead564ef62e55515ae3dac1c
Jon Janelle <jon@nextgengolf.org>
2018-06-27 16:55:13 -0400

    added option to load data from file

--- js/gmapsPolygon.js
+++ js/gmapsPolygon.js
@@ -24,15 +24,17 @@ function initMap(mapId) {
     return map;
 }
 
+
 gmapsPolygon.controller("PolygonController", function PolygonController($scope) {
-    $scope.colors = ["#FF0000", "#00FF00", "#0000FF", "#00FFFF", "#FFFF00", "#FF00FF"];
-    $scope.colorClasses = ["color-red", "color-green", "color-blue", "color-teal", "color-yellow", "color-purple"];
+    $scope.colors = ["#FF0000", "#00FF00", "#0000FF", "#00FFFF", "#FFFF00", "#FF00FF", "#666666"];
+    $scope.colorClasses = ["color-red", "color-green", "color-blue", "color-teal", "color-yellow", "color-purple", "color-gray"];
+    $scope.inputLabels = [];
     $scope.startCoords = [
         new google.maps.LatLng(43.87761,-96.10608), 
         new google.maps.LatLng(38.60883,-102.2495), 
         new google.maps.LatLng(38.54844,-87.97311)
     ];
-    $scope.polygons = [initPolygon($scope.colors[0], $scope.startCoords)]; 
+    $scope.polygons = []; 
     $scope.map = initMap('map-canvas', $scope.polygons, $scope.colors[0], $scope.startCoords);
     $scope.getPolygonCoords = function() {
         for (var p = 0; p < $scope.polygons.length; p++) {
@@ -46,19 +48,47 @@ gmapsPolygon.controller("PolygonController", function PolygonController($scope) 
             document.getElementById('info-'+p).innerHTML = htmlStr;
         }
     }
-    
-    for (var i = 0; i < $scope.polygons.length; i++) {
-        $scope.polygons[i].setMap($scope.map);
-        google.maps.event.addListener($scope.polygons[i].getPath(), "insert_at", $scope.getPolygonCoords);
-        google.maps.event.addListener($scope.polygons[i].getPath(), "set_at", $scope.getPolygonCoords);
-    }
 
     $scope.addPolygon = function() {
         var nPolygons = $scope.polygons.length;
         var newPolygon = initPolygon($scope.colors[nPolygons], $scope.startCoords)
         $scope.polygons.push(newPolygon);
+        $scope.inputLabels.push("Polygon "+$scope.polygons.length);
         newPolygon.setMap($scope.map);
         google.maps.event.addListener(newPolygon.getPath(), "insert_at", $scope.getPolygonCoords);
         google.maps.event.addListener(newPolygon.getPath(), "set_at", $scope.getPolygonCoords);
     }
+
+    $scope.readFile = function(e) {
+        var file = e.target.files[0];
+        if (!file) {
+          return;
+        }
+        $scope.inputLabels.push(file.name);
+        var reader = new FileReader();
+        reader.onload = function(e) {
+          var contents = e.target.result;
+          $scope.drawPolygonFromFile(contents);
+        };
+        reader.readAsText(file);
+    }
+
+      $scope.drawPolygonFromFile = function(contents) {
+        var dataLines = contents.split("\n");
+        var coordArray = [];
+        for (var i = 0; i < dataLines.length; i++) {
+            var coords = dataLines[i].split(",");
+            if (coords.length != 2) { continue; }
+            var lat = parseFloat(coords[0].split(":")[1].trim());
+            var lng = parseFloat(coords[1].split(":")[1].trim());
+            coordArray.push(new google.maps.LatLng(lat, lng));
+        }
+        var newPolygon = initPolygon($scope.colors[ $scope.polygons.length], coordArray);
+        $scope.polygons.push(newPolygon);
+        newPolygon.setMap($scope.map);
+        google.maps.event.addListener(newPolygon.getPath(), "insert_at", $scope.getPolygonCoords);
+        google.maps.event.addListener(newPolygon.getPath(), "set_at", $scope.getPolygonCoords);
+        $scope.$apply()
+      }
+      document.getElementById('file-input').addEventListener('change', $scope.readFile, false);
 });
